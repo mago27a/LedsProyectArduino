b1afcf955f6d5854fe696b49fb41cd4e92132bba
Mago27a <master.drcd@hotmail.com>
2023-03-14 17:07:51 -0500

    add funtion velocityDelayLeds

--- App.ino
+++ App.ino
@@ -2,13 +2,15 @@
 led1=2; led2=3...led9=10;
 */
 
-int seriesLed[3][9] = {
+int seriesLed[5][9] = {
     {2, 3, 4, 5, 6, 7, 8, 9, 10},
     {3, 5, 7, 9, 0, 0, 0, 0, 0},
     {2, 4, 6, 8, 10, 0, 0, 0, 0},
     {2, 4, 10, 8, 7, 0, 0, 0, 0},
     {2, 8, 7, 3, 10, 7, 9, 4, 6},
 };
+
+int speedsDelay[] = {300, 600, 900, 1200, 1500, 1800, 2100, 2400};
 
 void setup()
 {
@@ -17,8 +19,7 @@ void setup()
 
 void loop()
 {
-
-    illunimateSerieLed();
+  illunimateSerieLed(seriesLed);
 }
 
 /*Metodos*/
@@ -42,27 +43,6 @@ void setMatrizToOutput(int arr[5][9])
     }
 }
 
-int obtener_valor_aleatorio()
-{
-    int indice = random(9);
-    return valores[indice];
-}
-
-void serie1()
-{
-    int i = 0;
-
-    for (int i = 0; i < sizeof(ledPin); i++)
-    {
-        // digitalWrite(ledPin[i], HIGH);
-        // delay(500);
-        // digitalWrite(ledPin[i], LOW);
-        // delay(500);
-
-        onLed(i, 500);
-        offLed(i, 500);
-    }
-}
 
 void illunimateSerieLed(int arr[][9])
 {
@@ -72,8 +52,8 @@ void illunimateSerieLed(int arr[][9])
         for (int j = 0; j < 9; i++)
         {
             pin = arr[i][j];
-            onLed(pin, 1000);
-            offLed(pin, 1000);
+            onLed(pin, velocityDelayLeds(speedsDelay));
+            offLed(pin, velocityDelayLeds(speedsDelay));
         }
     }
 }
@@ -103,4 +83,14 @@ void offLed(int pin, int msTime)
         return;
     digitalWrite(pin, LOW);
     delay(msTime);
+}
+
+int velocityDelayLeds(int arr[])
+{
+    int longArr = sizeof(arr);
+    int index=0;
+    int currentValue=arr[index];  
+
+    return currentValue;
+     
 }
